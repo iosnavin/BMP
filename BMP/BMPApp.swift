32a043e255ccea24c8bc64b6b06b9c9168a535d8
Naveen <ios.nreddy@gmail.com>
2021-11-18 15:14:02 +0530

    Initial Commit

---
+++ BMP/BMPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMPApp.swift
+//  BMP
+//
+//  Created by Naveen Reddy on 18/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BMPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
